6fc9404520d4e8ebe83d384f7fba3ff2b489ce90
Prakhar Johri <johriprakhar@gmail.com>
2024-03-19 00:53:56 +0530

    debug

--- wp-content/plugins/woocommerce-pdf-invoices-packing-slips/templates/Simple/invoice.php
+++ wp-content/plugins/woocommerce-pdf-invoices-packing-slips/templates/Simple/invoice.php
@@ -128,7 +128,7 @@
 				
 				<td class="price"><?php echo $item['single_price']; ?></td>
 				<td class="quantity"><?php echo $item['quantity']; ?></td>
-				<td class="quantity"><?php echo $item['single_line_tax']; ?></td>
+				<td class="quantity"><?php echo $item['line_tax']; ?></td>
 				<td class="price"><?php echo $item['order_price']; ?></td>
 			</tr>
 
